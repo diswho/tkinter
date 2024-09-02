4ed08118ffcd314184e672213d06a7c0bba533a2
diswho <discover.who@gmail.com>
2024-09-02 15:26:26 +0700

    frame

--- frame.py
+++ frame.py
@@ -4,6 +4,12 @@ from PIL import ImageTk, Image
 root = Tk()
 root.title("My Tkinter Application")
 
+frame= LabelFrame(root,text="This is my frame...",padx=15,pady=5)
+frame.pack(padx=100,pady=10)
 
+b1=Button(frame,text="Quit",command=root.quit)
+b2=Button(frame,text="Click",command=root.quit)
+b1.grid(row=0,column=0)
+b2.grid(row=0,column=1)
 
 root.mainloop()
